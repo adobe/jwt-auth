b72e57c5e7473e24fb09f138e9e50ab97e359597
Hiren Shah <hireshah@adobe.com>
2019-02-19 15:39:54 -0700

    metaScopes as comma separated string

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function authorize(options) {
   !orgId ? errors.push('orgId') : '';
   !clientSecret ? errors.push('clientSecret') : '';
   !privateKey ? errors.push('privateKey') : '';
-  metaScopes.length === 0 ? errors.push('metaScopes') : '';
+  (!metaScopes || metaScopes.length === 0) ? errors.push('metaScopes') : '';
   if (errors.length > 0) {
     return Promise.reject(
       new Error('Required parameter(s) ' + errors.join(', ') + ' are missing')
