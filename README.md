2c069d2ab7472237ddc3d0e59cbd620b5bf7bec5
Simon MacDonald <simon.macdonald@gmail.com>
2019-02-19 09:43:28 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# @adobe/jwtauth
+
+Retrieve an Adobe bearer token via the JWT path
+
+## Goals
+
+It is a good idea to provide a mission statement for your project, enshrining
+what the project wants to accomplish so that as more people join your project
+everyone can work in alignment.
+
+## Non-Goals
+
+It is also a good idea to declare what are _not_ goals of the project to prevent
+potential feature creep.
+
+### Installation
+
+Instructions for how to download/install the code onto your machine.
+
+Example:
+
+```
+npm install @adobe/jwtauth
+```
+
+### Usage
+
+Usage instructions for your code.
+
+Example:
+
+```
+const auth = require('@adobe/jwtauth');
+
+auth(options).then(token => console.log(token));
+```
+
+### Contributing
+
+Contributions are welcomed! Read the [Contributing Guide](CONTRIBUTING.md) for more information.
+
+### Licensing
+
+This project is licensed under the Apache V2 License. See [LICENSE](LICENSE) for more information.
---
+++ index.js
@@ -0,0 +1,58 @@
+/*
+Copyright 2019 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
+const FormData = require('form-data');
+
+function authorize(options) {
+  let {
+    clientId,
+    technicalAccountId,
+    orgId,
+    clientSecret,
+    privateKey,
+    metaScopes = [],
+    ims = 'https://ims-na1.adobelogin.com'
+  } = options;
+
+  const jwtPayload = {
+    exp: Math.round(87000 + Date.now() / 1000),
+    iss: orgId,
+    sub: technicalAccountId,
+    aud: `${ims}/c/${clientId}`
+  };
+
+  for (let i = 0; i < metaScopes.length; i++) {
+    if (metaScopes.indexOf('https') > -1) {
+      jwtPayload[metaScopes[i]] = true;
+    } else {
+      jwtPayload[`${ims}/s/${metaScopes[i]}`] = true;
+    }
+  }
+
+  const token = jwt.sign(jwtPayload, privateKey, { algorithm: 'RS256' });
+
+  const form = new FormData();
+  form.append('client_id', clientId);
+  form.append('client_secret', clientSecret);
+  form.append('jwt_token', token);
+
+  const postOptions = {
+    method: 'POST',
+    body: form,
+    headers: form.getHeaders()
+  };
+
+  return fetch(`${ims}/ims/exchange/jwt/`, postOptions).then(res => res.json());
+}
+
+module.exports = authorize;
