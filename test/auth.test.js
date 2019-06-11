43293dc2695e8671286757889982a7055cb0da67
Shazron Abdullah <shaz@adobe.com>
2019-06-11 19:29:41 +0800

    Fixes #9 - unify errors (#11)
    * Fixes #9 - Make rejection values consistent
    
    * Add node version requirement to be >= 6
    
    For ES6 features
    
    * npm audit fix
    
    * Add unified error tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,3 +1,35 @@
+// MOCKS ////////////////////////////////////////////
+
+const mockAccessToken = 'asdasdasd'
+
+let jwt = require('jsonwebtoken')
+let jwtActual = require.requireActual('jsonwebtoken')
+jest.mock('jsonwebtoken', () => jest.fn())
+
+let mockResultSuccess = Promise.resolve({
+  ok: true,
+  json: () => Promise.resolve(
+    { access_token: mockAccessToken, expires_in: 123456 }
+  ) 
+})
+let mockResultFailure = Promise.resolve({
+  ok: false,
+  json: () => Promise.resolve(
+    { error: 'my_error_code', error_description: 'This is the error description.' }
+  ) 
+})
+let mockResultFailureUnknown = Promise.resolve({
+  ok: false,
+  json: () => Promise.resolve(
+    { foo: 'bar', baz: 'faz' }
+  ) 
+})
+
+let fetch = require('node-fetch')
+jest.mock('node-fetch', () => jest.fn())
+
+// ////////////////////////////////////////////
+
 const auth = require('../index');
 
 const clientId = '9x4f87e5xxxf46x68f68x46ee6ef2738';
@@ -10,19 +42,19 @@ const privateKey = 'aalsdjfajsldjfalsjkdfa;lsjf;aljs';
 describe('Validate input', () => {
   test('all parameters missing', () => {
     expect.assertions(1);
-    return expect(auth({})).rejects.toThrow();
+    return expect(auth({})).rejects.toThrow('Required parameter(s) clientId, technicalAccountId, orgId, clientSecret, privateKey, metaScopes are missing');
   });
   test('missing clientId', () => {
     expect.assertions(1);
     return expect(
       auth({ clientSecret, technicalAccountId, orgId, metaScopes, privateKey })
-    ).rejects.toThrow();
+    ).rejects.toThrow('Required parameter(s) clientId are missing');
   });
   test('missing clientSecret', () => {
     expect.assertions(1);
     return expect(
       auth({ clientId, technicalAccountId, orgId, metaScopes, privateKey })
-    ).rejects.toThrow();
+    ).rejects.toThrow('Required parameter(s) clientSecret are missing');
   });
   test('missing technicalAccountId', () => {
     expect.assertions(1);
@@ -34,7 +66,7 @@ describe('Validate input', () => {
         metaScopes,
         privateKey
       })
-    ).rejects.toThrow();
+    ).rejects.toThrow('Required parameter(s) technicalAccountId are missing');
   });
   test('missing orgId', () => {
     expect.assertions(1);
@@ -46,7 +78,7 @@ describe('Validate input', () => {
         metaScopes,
         privateKey
       })
-    ).rejects.toThrow();
+    ).rejects.toThrow('Required parameter(s) orgId are missing');
   });
   test('missing metaScopes', () => {
     expect.assertions(1);
@@ -58,7 +90,7 @@ describe('Validate input', () => {
         orgId,
         privateKey
       })
-    ).rejects.toThrow();
+    ).rejects.toThrow('Required parameter(s) metaScopes are missing');
   });
   test('missing privateKey', () => {
     expect.assertions(1);
@@ -70,11 +102,22 @@ describe('Validate input', () => {
         orgId,
         metaScopes
       })
-    ).rejects.toThrow();
+    ).rejects.toThrow('Required parameter(s) privateKey are missing');
   });
 });
 
 describe('Sign with invalid primary key', () => {
+  beforeEach(() => {
+    jwt.sign = jest.fn().mockImplementation((...args) => {
+      // call actual jwt module, not mocked version
+      return jwtActual.sign(...args)
+    })
+  })
+
+  afterEach(() => {
+    jwt.sign.mockClear()
+  })
+
   test('invalid primary key', () => {
     expect.assertions(1);
     return expect(
@@ -86,6 +129,62 @@ describe('Sign with invalid primary key', () => {
         metaScopes,
         privateKey
       })
-    ).rejects.toThrow();
+    ).rejects.toThrow('error:0906D06C:PEM routines:PEM_read_bio:no start line');
+  });
+});
+
+describe('Fetch jwt', () => {
+  beforeEach(() => {
+    jwt.sign = jest.fn().mockImplementation(() => {
+      return 'my_jwt_token'
+    })
+  })
+
+  afterEach(() => {
+    jwt.sign.mockClear()
+  })
+
+  test('valid jwt', () => {
+    fetch.mockImplementation(()=> mockResultSuccess)
+    return expect(
+      auth({
+        clientId,
+        clientSecret,
+        technicalAccountId,
+        orgId,
+        metaScopes,
+        privateKey
+      })
+    ).resolves.toEqual({"access_token": mockAccessToken, "expires_in": 123456});
+  });
+
+  test('invalid jwt, expected endpoint error', () => {
+    expect.assertions(1);
+    fetch.mockImplementation(()=> mockResultFailure)
+    return expect(
+      auth({
+        clientId,
+        clientSecret,
+        technicalAccountId,
+        orgId,
+        metaScopes,
+        privateKey
+      })
+    ).rejects.toThrow('my_error_code: This is the error description.');
+  });
+
+  test('invalid jwt, unknown error', () => {
+    expect.assertions(1);
+    fetch.mockImplementation(()=> mockResultFailureUnknown)    
+    return expect(
+      auth({
+        clientId,
+        clientSecret,
+        technicalAccountId,
+        orgId,
+        metaScopes,
+        privateKey
+      })
+    ).rejects.toThrow('An unknown error occurred while swapping jwt. The response is as follows: {\"foo\":\"bar\",\"baz\":\"faz\"}');
   });
 });
